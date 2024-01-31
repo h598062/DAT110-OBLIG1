59081cea88269ce548aba33113cb0403942c6283
Storm <sangolts@gmail.com>
2024-01-31 11:48:11 +0100

    asd

--- src/main/java/no/hvl/dat110/system/controller/Controller.java
+++ src/main/java/no/hvl/dat110/system/controller/Controller.java
@@ -44,9 +44,7 @@ public class Controller {
 		// setup stop methods in the RPC middleware
 		RPCClientStopStub stopdisplay = new RPCClientStopStub(displayclient);
 		RPCClientStopStub stopsensor = new RPCClientStopStub(sensorclient);
-
-		// TODO - START
-
+		
 		// create local display and sensor stub objects
 		display = new DisplayStub(displayclient);
 		sensor = new SensorStub(sensorclient);
@@ -62,8 +60,6 @@ public class Controller {
         // read value from sensor using RPC and write to display using RPC
 		String temperatur = Integer.toString(sensor.read());
 		display.write(temperatur);
-
-		// TODO - END
 
 		stopdisplay.stop();
 		stopsensor.stop();
--- src/main/java/no/hvl/dat110/system/display/DisplayImpl.java
+++ src/main/java/no/hvl/dat110/system/display/DisplayImpl.java
@@ -40,8 +40,6 @@ public class DisplayImpl extends RPCRemoteImpl {
 		// implement unmarshalling, call, and marshall for write RPC method
 		// look at how this is done in the SensorImpl class for the read method
 
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - END
 
